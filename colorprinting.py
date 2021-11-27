e247db984633f3beb413de444a7cd9bdfd4bf59e
Tejas Kumar Singh <95142956+tejaskrsingh@users.noreply.github.com>
2021-11-27 23:55:26 +0530

    Create colorprinting.py

---
+++ colorprinting.py
@@ -0,0 +1,6 @@
+print('This is Normal text') #Text is normal
+print("\033[1;31;40m This is red text") #1 is used for normal font style 
+print("\033[1;32;40m This is green text") #32 is color code for green text
+print("\033[1;33;40m This is yellow text") #40m is used for background color
+print("\033[1;34;40m This is blue text") #\033 is escape code for color which is always constant
+print("\033[1;30;47m This is black text with white background")
